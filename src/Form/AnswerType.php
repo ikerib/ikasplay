e8694464c29a1a5b28e3207292951667f1de0644
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2019-06-06 23:46:01 +0200

    easyadmin

--- src/Form/AnswerType.php
+++ src/Form/AnswerType.php
@@ -3,30 +3,25 @@
 namespace App\Form;
 
 use App\Entity\Answer;
-use App\Entity\Question;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class QuestionType extends AbstractType
+class AnswerType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('name')
-            ->add('answers', CollectionType::class, [
-                'entry_type' => AnswerType::class,
-                'entry_options' => ['label'=>false],
-                'allow_add' => true
-            ])
+            ->add('correct')
+            ->add('question')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Question::class,
+            'data_class' => Answer::class,
         ]);
     }
 }
